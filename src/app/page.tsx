8ccad7ee66f969536882ab7e61338cd91add2f13
rajanirewa <rajanirinfo@gmail.com>
2023-10-13 16:12:33 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import Home from './Components/Home'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
 
 export default function Page() {
   return (
  <>
      <div id="root" className='bg-black'>
     <Home/>
-  </div>
+    </div>
  </>
   )
   }
